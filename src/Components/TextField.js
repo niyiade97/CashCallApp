b9d31476e3f7f0dfc7bcdb5ab98f653831046c45
adeniyi <niyiade97@gmail.com>
2021-11-17 12:14:42 +0100

    made changes to the textfield component

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function TextField(props) {
+function TextField({label, onChange, disabled, placeholder, name, type}) {
 
     const handleOnchange = (e) =>{
         e.preventDefault();
-        props.onChange(e.target.value);
+        onChange(e.target.value);
     }
     return (
         <div className="w-2/4 px-4 py-3">
             <div className="w-full text-color5">
-                <label className="font-normal text-lg">{props.label}</label> 
-                <input className={`rounded-full w-full mt-4 h-14 px-4 font-semibold ${props.disabled ? "bg-color12" : "bg-white border border-color5 text-color13 placeholder-color13"}`} type={props.type} name={props.name} placeholder={props.placeholder} onChange={handleOnchange} disabled={props.disabled ? true : false} />
+                <label className="font-normal text-lg">{label}</label> 
+                <input className={`rounded-full w-full mt-4 h-14 px-4 font-semibold ${disabled ? "bg-color12" : "bg-white border border-color5 text-color13 placeholder-color13"}`} type={type} name={name} placeholder={placeholder} onChange={handleOnchange} disabled={disabled ? true : false} />
             </div>
         </div>
         
